0c6492bf88b49b9a7b0506e6f55ab7cc862662cc
Daniel Persson <daniel.persson@spoon.se>
2015-11-24 15:09:50 +0100

    Setup world scripts with debug to show how many tiles. Create Tiles.

---
+++ BasebuildingUnity/Assets/Models/Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile {
+
+	public enum TileType { Empty, Floor };
+
+	TileType type = TileType.Empty;
+
+	LooseObject looseObject;
+	InstalledObject installedObject;
+
+	World world;
+	//coordinates in the world
+	int x; 
+	int y;
+
+	/*constructor (Konstruktorn är metoden som anropas när du gör ett nytt objekt. 
+	#Så det passar bra för initialiseringar och sånt.  Man måste alltid ha en konstruktor,
+	#om du inte gör en själv så skapas en default konstruktor som inte tar några argument alls. )*/
+	public Tile( World world, int x, int y) {
+		this.world = world;
+		this.x = x;
+		this.y = y;
+	}
+}
